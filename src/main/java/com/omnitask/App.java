1b9653a8921a8c7bef4253aacadafd9866c24fa9
Faizuddin Safa <safarajuk19@gmail.com>
2026-04-14 22:52:49 +0600

    TAsk MAnager works success: stores in file

--- src/main/java/com/omnitask/App.java
+++ src/main/java/com/omnitask/App.java
@@ -1,6 +1,10 @@
 package com.omnitask;
 
 //import javafx.application.Application;
+
+import java.io.IOException;
+import java.util.List;
+
 //import javafx.fxml.FXMLLoader;
 //import javafx.scene.Parent;
 //import javafx.scene.Scene;
@@ -11,7 +15,7 @@ package com.omnitask;
 /**
  * JavaFX App
  */
-public class App extends Application {
+public class App  {
 //
 //    private static Scene scene;
 //
@@ -31,8 +35,13 @@ public class App extends Application {
 //        return fxmlLoader.load();
 //    }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         //launch();
+        TaskManager.addTask("Safa", "WowQuizCancelled", "Quiz cancellation notice makes everyone to enjpy the week, though finals are two weeks later, we get 2 days PL.):", "15-04-2026");
+        List<String[]> tasks = TaskManager.loadTask("Safa");
+        for (String[] t : tasks) {
+            System.out.println(t[0]+ "|"+t[1]+ t[2]+ "|"+t[3]);
+        }
     }
 
 }
